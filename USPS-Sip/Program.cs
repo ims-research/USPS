684557656adbed10a5184bd9217cf1d501afff49
Richard Spiers <richard@thoughtexpress.com>
2012-10-11 11:15:45 +0200

    Added SIP project to handle SIP communication

---
+++ USPS-Sip/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+using SIPLib.SIP;
+using SIPLib.Utils;
+using log4net;
+
+namespace USPS_Sip
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(SIPApp));
+        private static readonly ILog SessionLog = LogManager.GetLogger("SessionLogger");
+        private static SIPApp _app;
+        private const string ServiceLocation = "C:\\Users\\richard\\Documents\\GitHub\\USPS\\USPS\\Resources\\Services\\";
+
+        public static SIPStack CreateStack(SIPApp app, string proxyIp = null, int proxyPort = -1)
+        {
+            SIPStack myStack = new SIPStack(app, "USPS");
+            if (proxyIp != null)
+            {
+                myStack.ProxyHost = proxyIp;
+                myStack.ProxyPort = (proxyPort == -1) ? 5060 : proxyPort;
+            }
+            return myStack;
+        }
+
+        public static TransportInfo CreateTransport(string listenIp, int listenPort)
+        {
+            return new TransportInfo(IPAddress.Parse(listenIp), listenPort, System.Net.Sockets.ProtocolType.Udp);
+        }
+
+        static void AppResponseRecvEvent(object sender, SipMessageEventArgs e)
+        {
+            Log.Info("Response Received:" + e.Message);
+            Message response = e.Message;
+            string requestType = response.First("CSeq").ToString().Trim().Split()[1].ToUpper();
+            switch (requestType)
+            {
+                case "INVITE":
+                case "REGISTER":
+                case "BYE":
+                default:
+                    Log.Info("Response for Request Type " + requestType + " is unhandled ");
+                    break;
+            }
+        }
+
+        static void AppRequestRecvEvent(object sender, SipMessageEventArgs e)
+        {
+            Log.Info("Request Received:" + e.Message);
+            Message request = e.Message;
+            switch (request.Method.ToUpper())
+            {
+                case "INVITE":
+                case "BYE":
+                case "ACK":
+                case "CANCEL":
+                case "MESSAGE":
+                    {
+                        _app.Useragents.Add(e.UA);
+                        Message m = e.UA.CreateResponse(200, "OK");
+                        e.UA.SendResponse(m);
+                        if (request.First("Content-Type").ToString().ToUpper().Equals("APPLICATION/SERVLIST+XML"))
+                        {
+                            ProcessMessageBody(request.Body);
+                        }
+                        break;
+                    }
+                case "OPTIONS":
+                case "REFER":
+                case "SUBSCRIBE":
+                case "NOTIFY":
+                case "PUBLISH":
+                case "INFO":
+                default:
+                    {
+                        Log.Info("Request with method " + request.Method.ToUpper() + " is unhandled");
+                        break;
+                    }
+            }
+        }
+
+        private static void ProcessMessageBody(string messageBody)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            string[] services = messageBody.Split('\n');
+            foreach (string service in services)
+            {
+                xmlDoc.LoadXml(messageBody);
+                XmlNodeList guids = xmlDoc.GetElementsByTagName("GUID");
+                string serviceGUID = guids[0].InnerText;
+                xmlDoc.Save(ServiceLocation + serviceGUID + ".xml");    
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TransportInfo localTransport = CreateTransport(Helpers.GetLocalIP(), 7202);
+            _app = new SIPApp(localTransport);
+            _app.RequestRecvEvent += new EventHandler<SipMessageEventArgs>(AppRequestRecvEvent);
+            _app.ResponseRecvEvent += new EventHandler<SipMessageEventArgs>(AppResponseRecvEvent);
+            const string scscfIP = "scscf.open-ims.test";
+            const int scscfPort = 6060;
+            SIPStack stack = CreateStack(_app, scscfIP, scscfPort);
+            stack.Uri = new SIPURI("usps@open-ims.test");
+            Console.ReadKey();
+        }
+    }
+}
